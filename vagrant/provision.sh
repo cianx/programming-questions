8070541e81c6c1d17acbfed34ee8e4b614ca64c7
Cian Montgomery <cian@cianmontgomery.com>
2016-10-09 15:41:44 -0700

    update vagrant to 16.04

--- vagrant/provision.sh
+++ vagrant/provision.sh
@@ -4,7 +4,16 @@ STARTTIME=$(date +%s)
 export PROJECT_BASH_OPTS=-x
 apt-get update
 apt-get upgrade
-apt-get install -y build-essentials cmake gcc g++ python python3
+apt-get install -y build-essentials 
+
+# c/c++
+apt-get install -y cmake gcc g++
+
+#python
+apt-get install -y python
+
+#python3
+apt-get install -y python3
 
 # rust
 curl -sSf https://static.rust-lang.org/rustup.sh | sh
@@ -14,9 +23,15 @@ add-apt-repository ppa:ubuntu-lxc/lxd-stable
 apt-get update
 apt-get install -y golang
 
-CMD="source /home/vagrant/vagrant/bashrc"
-if ! grep -Fxq "$CMD" /home/vagrant/.bashrc; then
-    echo $CMD >> /home/vagrant/.bashrc
+USER=ubuntu
+BASHRC=/home/$USER/.bashrc
+if [ ! -f $BASHRC ]; then
+	echo "" >> $BASHRC
+fi
+
+CMD="source /vagrant/bashrc"
+if ! grep -Fxq "$CMD" $BASHRC; then
+    echo $CMD >> $BASHRC
 fi
 
 
