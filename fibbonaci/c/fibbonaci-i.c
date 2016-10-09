e2650d6730cac306e8927f17c93f668fc256a661
Cian Montgomery <cian@cianmontgomery.com>
2016-10-09 16:55:17 -0700

    whitespace cleanup.

--- fibbonaci/c/fibbonaci-i.c
+++ fibbonaci/c/fibbonaci-i.c
@@ -7,24 +7,24 @@
 
 int main(int argc, char**argv) {
 
-	int count = 0;
-	if(argc < 2|| argc > 2) {
-		printf("Usage: fibbonaci-i <count>\n");
-		return -1;
-	}
-	count = atoi(argv[1]);
+    int count = 0;
+    if(argc < 2|| argc > 2) {
+        printf("Usage: fibbonaci-i <count>\n");
+        return -1;
+    }
+    count = atoi(argv[1]);
 
-	int first  = 0;
-	int second = 1; 
-
-
-	for( int i = 0; i < count; ++i) {
-		printf("%d\n", second);
-		int next = first + second;
-		first = second;
-		second = next;
-	}
+    int first  = 0;
+    int second = 1;
 
 
-	return 0;
+    for( int i = 0; i < count; ++i) {
+        printf("%d\n", second);
+        int next = first + second;
+        first = second;
+        second = next;
+    }
+
+
+    return 0;
 }
